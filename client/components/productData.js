f9c966c4ace322b71ec5e8a2ff68d80278f062cb
sayeedkhannabil <sayeedkhannabil@gmail.com>
2024-05-05 23:32:39 -0500

    axios works with paginaton

---
+++ client/components/productData.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import 'regenerator-runtime/runtime';
+
+const getProductData = async () => {
+    try {
+        const response = await axios.get('http://localhost:3000/api/products');
+        const values = response.data;
+
+        return values;
+    }
+    catch (error) {
+        console.error(error);
+
+        return [];
+    }
+    
+}
+
+export default getProductData;
